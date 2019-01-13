be1c08acf9955e7797afdeb8bf70d61d4f360fd4
Petros Kyriakou <9pitops@gmail.com>
2019-01-13 13:30:33 +0200

    updated dependencies to latest

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -3,5 +3,5 @@ import 'firebase/firestore'
 import firebaseConfig from './firebaseConfig'
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
-
+firebaseApp.firestore().settings({ timestampsInSnapshots: true })
 export default firebaseApp.firestore()
